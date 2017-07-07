1c8fdfeb01e2589b6e70da524ce3803823eb0b20
yucccc <13450003105@163.com>
2017-07-07 21:58:34 +0800

    reset

--- routes/goods.js
+++ routes/goods.js
@@ -1,9 +1,9 @@
-// 爬取京东手机列表
 var express = require('express');
 var router = express.Router()
 var mongoose = require('mongoose')
 var Computer = require('../models/computers')
 mongoose.connect('mongodb://127.0.0.1:27017/mymall')
+// 电脑列表
 router.get('/computer', function (req, res, next) {
     Computer.find({}, function (err, doc) {
         if (err) {
